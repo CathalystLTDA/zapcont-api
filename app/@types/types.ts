250267432121bbd9a9e2bd2fff188af0ec35c5ee
Eric <ericmathias2012@gmail.com>
2025-03-28 16:24:51 -0300

    (trying) to create route for emission of nfse

---
+++ app/@types/types.ts
@@ -0,0 +1,124 @@
+type Provider = {
+  id: string;
+  tradeName: string;
+  openningDate: string; // data no formato ISO 8601
+  taxRegime: "Isento" | "MicroempreendedorIndividual" | "SimplesNacional" | "LucroPresumido" | "LucroReal";
+  specialTaxRegime: "Automatico" | "Nenhum" | "MicroempresaMunicipal" | "Estimativa" | "SociedadeDeProfissionais" | "Cooperativa" | "MicroempreendedorIndividual" | "MicroempresarioEmpresaPequenoPorte";
+  legalNature: "EmpresaPublica" | "SociedadeEconomiaMista" | "SociedadeAnonimaAberta" | "SociedadeAnonimaFechada" | "SociedadeEmpresariaLimitada" | "SociedadeEmpresariaEmNomeColetivo" | "SociedadeEmpresariaEmComanditaSimples" | "SociedadeEmpresariaEmComanditaporAcoes" | "SociedadeemContaParticipacao" | "Empresario" | "Cooperativa" | "ConsorcioSociedades" | "GrupoSociedades" | "EmpresaDomiciliadaExterior" | "ClubeFundoInvestimento" | "SociedadeSimplesPura" | "SociedadeSimplesLimitada" | "SociedadeSimplesEmNomeColetivo" | "SociedadeSimplesEmComanditaSimples" | "EmpresaBinacional" | "ConsorcioEmpregadores" | "ConsorcioSimples" | "EireliNaturezaEmpresaria" | "EireliNaturezaSimples" | "ServicoNotarial" | "FundacaoPrivada" | "ServicoSocialAutonomo" | "CondominioEdilicio" | "ComissaoConciliacaoPrevia" | "EntidadeMediacaoArbitragem" | "PartidoPolitico" | "EntidadeSindical" | "EstabelecimentoBrasilFundacaoAssociacaoEstrangeiras" | "FundacaoAssociacaoDomiciliadaExterior" | "OrganizacaoReligiosa" | "ComunidadeIndigena" | "FundoPrivado" | "AssociacaoPrivada";
+  economicActivities: Array<{ type: "Atividades da Empresa"; code: number }>;
+  companyRegistryNumber: number;
+  regionalTaxNumber: number;
+  municipalTaxNumber: string;
+  issRate: number;
+  federalTaxDetermination: "NotInformed" | "Default" | "SimplesNacional";
+  municipalTaxDetermination: "NotInformed" | "Default" | "SimplesNacional";
+  loginName: string;
+  loginPassword: string;
+  authIssueValue: string;
+  name: string;
+  federalTaxNumber: number;
+  email: string;
+  address: Address;
+  status: "Active" | "Inactive" | "Suspended";
+  type: "Jurídica" | "Física";
+  createdOn: string;
+  modifiedOn: string;
+};
+
+type Borrower = {
+  id: string;
+  name: string;
+  federalTaxNumber: number;
+  email: string;
+  address: Address;
+  status: "Active" | "Inactive" | "Suspended";
+  type: "Jurídica" | "Física";
+  createdOn: string;
+  modifiedOn: string;
+};
+
+type Address = {
+  country: string;
+  postalCode: string;
+  street: string;
+  number: string;
+  additionalInformation: string;
+  district: string;
+  city: City;
+};
+
+type City = {
+  code: string;
+  name: string;
+};
+
+type Location_t = {
+  state: string;
+  country: string;
+  postalCode: string;
+  street: string;
+  number: string;
+  district: string;
+  additionalInformation: string;
+  city: City;
+};
+
+type ActivityEvent = {
+  name: string;
+  startOn: string;
+  endOn: string;
+  atvEvId: string;
+};
+
+type ApproximateTax = {
+  source: string;
+  version: string;
+  totalRate: number;
+};
+
+export type NfseBody = {
+  id: string;
+  environment: 'Development' | 'Production' | 'Staging';
+  flowStatus: 'CancelFailed' | 'IssueFailed' | 'Issued' | 'Cancelled' | 'PullFromCityHall' | 'WaitingCalculateTaxes' | 'WaitingDefineRpsNumber' | 'WaitingSend' | 'WaitingSendCancel' | 'WaitingReturn' | 'WaitingDownload';
+  flowMessage: string;
+  provider: Provider;
+  borrower: Borrower;
+  externalId: string;
+  batchNumber: number;
+  batchCheckNumber: string;
+  number: number;
+  checkCode: string;
+  status: 'Error' | 'None' | 'Created' | 'Issued' | 'Cancelled';
+  rpsType: 'Rps' | 'RpsMista' | 'Cupom';
+  rpsStatus: 'Normal' | 'Canceled' | 'Lost';
+  taxationType: 'None' | 'WithinCity' | 'OutsideCity' | 'Export' | 'Free' | 'Immune' | 'SuspendedCourtDecision' | 'SuspendedAdministrativeProcedure' | 'OutsideCityFree' | 'OutsideCityImmune' | 'OutsideCitySuspended' | 'OutsideCitySuspendedAdministrativeProcedure' | 'ObjectiveImune';
+  issuedOn: string;
+  cancelledOn: string;
+  rpsSerialNumber: string;
+  rpsNumber: number;
+  cityServiceCode: string;
+  federalServiceCode: string;
+  description: string;
+  servicesAmount: number;
+  deductionsAmount: number;
+  discountUnconditionedAmount: number;
+  discountConditionedAmount: number;
+  baseTaxAmount: number;
+  issRate: number;
+  issTaxAmount: number;
+  irAmountWithheld: number;
+  pisAmountWithheld: number;
+  cofinsAmountWithheld: number;
+  csllAmountWithheld: number;
+  inssAmountWithheld: number;
+  issAmountWithheld: number;
+  othersAmountWithheld: number;
+  amountWithheld: number;
+  amountNet: number;
+  location: Location_t;
+  activityEvent: ActivityEvent;
+  approximateTax: ApproximateTax;
+  additionalInformation: string;
+  createdOn: string;
+  modifiedOn: string;
+}
---
+++ app/api/ServiceInvoices/route.ts
@@ -0,0 +1,37 @@
+import { NfseBody } from '@/app/@types/types';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    const { company_id, body }: { company_id: string, body: NfseBody } = await req.json()
+
+    if (!company_id || !body) {
+      return NextResponse.json({ error: 'Missing company_id or body' }, { status: 400 })
+    }
+    const response = await fetch(`${process.env.NFEIO_API_URL}/companies/${company_id}/serviceinvoices`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.NFEIO_API_KEY}`, // Certifique-se de ter sua chave de API configurada no .env
+      },
+      body: JSON.stringify(body),
+    })
+    const data = await response.json()
+    if (response.status === 400) {
+      return NextResponse.json({ error: 'Bad Request' }, { status: 400 })
+    }
+    if (response.status === 401) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+    if (response.status === 408) {
+      return NextResponse.json({ error: 'Time limit exceeded' }, { status: 408 })
+    }
+    if (!response.ok) {
+      return NextResponse.json({ status: response.status })
+    }
+
+    return NextResponse.json({ data: data, status: 202 }) // Retorna a resposta da API da NFE.io
+  } catch (error) {
+    return NextResponse.json({ error: 'Error processing', status: 500 }) // Retorna um erro genérico
+  }
+}
